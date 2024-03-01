d9a13662c1f3e629f32436ba5a8291d36f9565d4
rex-vero <142157808+rex-vero@users.noreply.github.com>
2024-03-01 23:48:39 +0330

    bootstarp, scss for css, font, images & js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,31 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
-    const search = document.getElementById('search').value.toLowerCase();
+    const search = document.getElementById('search');
     const searchDiv = document.getElementById('searcher');
+    const controller = new AbortController();
+    const signal = controller.signal;
     searchDiv.innerHTML = '';
-    fetch('https://fakestoreapi.com/products').then(res => { return res.json() }).then(data => {
+    if (search.value === '') {
+        searchDiv.classList.add('d-none');
+        controller.abort();
+        // error in abort()
+    }
+    fetch('https://fakestoreapi.com/products', { signal }).then(res => { return res.json() }).then(data => {
         data.map(item => {
-            if (item.title.toLowerCase().includes(search)) {
+            if (item.title.toLowerCase().includes(search.value.toLowerCase())) {
                 searchDiv.classList.remove('d-none');
                 const cards = document.createElement('div');
-                cards.classList.add('d-flex');
-                cards.innerHTML = `
-            <a href="./pages/account.html"
-            class="text-decoration-none white">
-            <img src="${item.image}" class="img-fluid w-25 card-img-top">
+                cards.innerHTML = `<a href="#" class="d-flex flex-column text-decoration-none">
+            <img src="${item.image}" class="img-fluid img align-self-center">
             <div class="card-body text-center">
-                    <h4 class="card-title">${item.title}</h4>
+                    <h5 class="card-title">${item.title}</h5>
             </div>
             <div class="card-footer">
                 <small class="text-body-secondary">Buy Now ${item.price}$ </small>
             </div>
             </a>`;
-                searchDiv.append(cards);
+                searchDiv.appendChild(cards);
             }
-            // else have problem
         })
     })
 });
@@ -32,8 +35,8 @@ fetch('https://fakestoreapi.com/products').then(res => { return res.json() }).th
         const cards = document.createElement('div');
         cards.classList.add('d-flex');
         cards.innerHTML = `
-    <a href="./pages/account.html"
-    class="text-decoration-none white">
+    <a href="#"
+    class="text-decoration-none">
     <img src="${item.image}" class="img-fluid w-25 card-img-top">
     <div class="card-body text-center">
             <h4 class="card-title">${item.title}</h4>
